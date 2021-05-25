297feb2495e715b65c861120f12956e743cca423
jhurst16 <jeremyhurst16@gmail.com>
2021-05-25 12:47:55 -0400

    added a main background for scrolling

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//this is for the scrolling background
+document.getElementById("body").onscroll = function scrollingBackgound() {  
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("background");
+    var xvalue = "center";
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+  }
+//scrolling background end
