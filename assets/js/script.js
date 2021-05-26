3a336aabfe19ad42f48dd84d0a41b612f6926395
Jeremy's commits <jeremyhurst16@gmail.com>
2021-05-25 22:11:57 -0400

    added a secondary background

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,11 @@ document.getElementById("body").onscroll = function scrollingBackgound() {
     target.style.backgroundPosition = xvalue + " " + yvalue + "px";
   }
 //scrolling background end
+document.getElementById("body").onscroll = function scrollingBackgound() {  
+  var scrolltotop = document.scrollingElement.scrollTop;
+  var target = document.getElementById("background2");
+  var xvalue = "center";
+  var factor = 0.5;
+  var yvalue = scrolltotop * factor;
+  target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+}
