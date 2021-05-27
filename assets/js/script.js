03cc24c1f94387afa1dae3ba8a755392a8011e65
charity.rogers <rogerscl116@gmail.com>
2021-05-26 22:09:31 -0500

    fetch data from youtube api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-var youTubeLi = document.getElementById("shadow");
+var youTubeDiv = document.getElementById("you-tube");
 var searchButton = document.getElementById("search-button");
+var inputEl = document.getElementById("input");
+var youTubeApi = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyDSLlhNJxicn6aZwfjtRXoKPBvHT599JFc&type=video&part=snippet&maxResults=6&videoEmbeddable=true&q=`;
 
 //this is for the scrolling background
 document.getElementById("body").onscroll = function scrollingBackgound() {  
@@ -12,12 +14,24 @@ document.getElementById("body").onscroll = function scrollingBackgound() {
   }
 //scrolling background end
 
-// event handler for search button
-$(document).ready(function() {
-  $("button").click(function() {
-    $(this).gameSearch();
+// function performed when search button is clicked
+$(searchButton).on("click", function(event) {
+  event.preventDefault();
+  youTubeDiv.innerHTML = "";
+  var inputValue = inputEl.value;
+  queryUrl = youTubeApi + inputValue;
+
+// fetch data from youtube api
+  fetch(queryUrl)
+  .then(function(response) {
+    return response.json();
+})
+.then(function(response) {
+  var videoId = response.items[0].id.videoId;
+  var videoTitle = response.items[0].snippet.title;
+
+  youTubeDiv.innerHTML = `<iframe width="400" height="300" src="https://www.youtube.com/embed/{videoId}" title="{videoTitle}" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
   });
 });
 
-// function performed when search button is clicked
-$("#searchButton")
