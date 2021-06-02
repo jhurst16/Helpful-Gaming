83edd1d667c6b57e29e08676aa8137c26e4f1d80
charity.rogers <rogerscl116@gmail.com>
2021-06-01 20:29:54 -0500

    update displayWiki and add displaySnippet functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,65 +3,67 @@ var wikiContainer = $("#wiki");
 var searchButton = $("#search-button");
 var inputEl = $("#input");
 var youTubeUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyDSLlhNJxicn6aZwfjtRXoKPBvHT599JFc&type=video&part=snippet&maxResults=6&videoEmbeddable=true&q=+super mario`;
-var wikiUrl = `https://www.mariowiki.com/api.php?action=query&list=search&format=json&srsearch=+super%20mario`
-
+var wikiUrl = `https://www.mariowiki.com/api.php?action=query&list=search&format=json&srsearch=`
 // function to display youTube videos
-var displayVideo = function(object){
+var displayVideo = function (object) {
   console.log(object)
   //represents one node
   var video = $('<div>')
   video.html(`<iframe width="300" height="200" src="https://www.youtube.com/embed/${object.id.videoId}" title="${object.snippet.title}" frameborder="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
   youTubeContainer.append(video)
 }
-
 // function performed when search button is clicked
-  searchButton.on("click", function(event) {
+searchButton.on("click", function (event) {
   event.preventDefault();
   youTubeContainer.html('')
   var inputValue = inputEl.val()
   queryUrl = youTubeUrl + inputValue;
-
-// fetch data from youtube api
+  // fetch data from youtube api
   fetch(queryUrl)
-  .then(function(response) {
-    return response.json();
-})
-.then(function(response) {
-  $.each(response.items, function(index, item){
-    displayVideo(item)
-  })
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      $.each(response.items, function (index, item) {
+        displayVideo(item)
+      })
+    });
 });
-
-// function to display wiki websites
-var displayWiki = function(object){
-  console.log(object)
+var displaySnippet = function (object) {
   //represents one node
-  var wiki = $('<div>')
-  wiki.html(json.query.search[0].title);
-  wikiContainer.append(wiki);
-
+  var wikiSnippet = $('<div>')
+  wikiSnippet.html(object.snippet);
+  wikiContainer.append(wikiSnippet);
 }
-searchButton.on("click", function(event) {
+// function to display wiki websites
+var displayWiki = function (wiki) {
+  console.log(wiki)
+  //represents one node
+  var wikiTitle = $('<div>')
+  wikiTitle.html(wiki.title);
+  wikiContainer.append(wikiTitle);
+  displaySnippet(wiki)
+}
+searchButton.on("click", function (event) {
   event.preventDefault();
   wikiContainer.html('')
   var inputValue = inputEl.val()
   queryUrl = wikiUrl + inputValue;
-
-// fetch data from youtube api
+  console.log(queryUrl)
+  // fetch data from youtube api
   fetch(queryUrl)
-  .then(function(response) {
-    return response.json();
-})
-.then(function(response) {
-  $.each(response.items, function(index, item){
-    displayWiki(item)
-  })
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+       const wikiResults= response.query.search.splice(0,6)
+       wikiResults.forEach(wikiTitle => {
+         displayWiki(wikiTitle)
+       })
+    });
 });
-
 //this is for the scrolling background
-document.getElementById("body").onscroll = function scrollingBackgound() {  
+document.getElementById("body").onscroll = function scrollingBackgound() {
   var scrolltotop = document.scrollingElement.scrollTop;
   var target = document.getElementById("background");
   var xvalue = "center";
