2fe417663f1844733a6a793d604eae722d133253
charity.rogers <rogerscl116@gmail.com>
2021-06-12 18:52:42 -0500

    update click function and localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var inputEl = $("#input");
 var historyContainer = $("#history-container")
 var youTubeUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBaHR_vdcyTqtzFyLY6v-Gj9QeThGAmExA&type=video&part=snippet&maxResults=4&videoEmbeddable=true&q=+super mario`;
 var wikiUrl = `https://www.mariowiki.com/api.php?action=query&list=search&format=json&srsearch=`
+var searchArray = JSON.parse(localStorage.getItem('userInput')) || [];
+
 // function to display youTube videos
 
 var displayVideo = function (object) {
@@ -20,19 +22,55 @@ searchButton.on("click", function (event) {
   event.preventDefault();
   youTubeContainer.html('')
   var inputValue = inputEl.val()
-  localStorage.setItem("userInput", JSON.stringify(inputValue))
-  var makeButton = $("button")
-  makeButton.html(inputValue)
-  historyContainer.append(makeButton)
-  queryUrl = youTubeUrl + inputValue;
+  queryUrl1 = youTubeUrl + inputValue;
+  // localStorage.setItem("userInput", JSON.stringify(inputValue))
+  // var data = JSON.parse(localStorage.getItem('userInput'))
+  //  for (var i = searchArray.length - 1; i >= 0; i--) {
+  //  var makeButton = $('searchHistoryButton')
+  //  makeButton.textContent = searchArray[i];
+  //  historyContainer.append(makeButton)
+   
+   var renderBtns = function(searchArray) {
+     searchArray.push('inputValue');
+     localStorage.setItem("userInput", JSON.stringify(inputValue));
+     searchArray.forEach(makeButton);
+      var makeButton = $('searchHistoryButton')
+      makeButton.html(searchArray[i]);
+      historyContainer.append(makeButton);
+   
 
-  fetch(queryUrl)
+   renderBtns();
+  // for (var i = searchHistoryButton.length - 1; i >= 0; i--) {
+  //   var btn = document.createElement('button');
+  //   btn.setAttribute('type', 'button');
+  //   btn.classList.add('history-btn', 'btn-history');
+  //   // `data-search` allows access to city name when click handler is invoked
+  //   btn.setAttribute('data-search', searchHistory[i]);
+  //   btn.textContent = searchHistory[i];
+  //   searchHistoryContainer.append(btn);
+  }
+
+  fetch(queryUrl1)
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
       $.each(response.items, function (index, item) {
         displayVideo(item)
+      })
+    });
+    wikiContainer.html('')
+  queryUrl2 = wikiUrl + inputValue;
+
+  // fetch data from youtube api
+  fetch(queryUrl2)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      const wikiResults = response.query.search.splice(0, 4)
+      wikiResults.forEach(wikiTitle => {
+        displayWiki(wikiTitle)
       })
     });
 });
@@ -51,23 +89,6 @@ var displayWiki = function (wiki) {
   wikiContainer.append(wikiTitle);
   displaySnippet(wiki)
 }
-searchButton.on("click", function (event) {
-  event.preventDefault();
-  wikiContainer.html('')
-  var inputValue = inputEl.val()
-  queryUrl = wikiUrl + inputValue;
-  // fetch data from youtube api
-  fetch(queryUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      const wikiResults = response.query.search.splice(0, 4)
-      wikiResults.forEach(wikiTitle => {
-        displayWiki(wikiTitle)
-      })
-    });
-});
 
 //this is for the scrolling background
 document.getElementById("body").onscroll = function scrollingBackgound() {
