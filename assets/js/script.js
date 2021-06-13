6e57f857abfbc5c140e4ca79f5c2bf030dfa06a1
charity.rogers <rogerscl116@gmail.com>
2021-06-12 22:39:17 -0500

    fix search button and localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var wikiContainer = $("#wiki");
 var searchButton = $("#search-button");
 var inputEl = $("#input");
 var historyContainer = $("#history-container")
-var youTubeUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBaHR_vdcyTqtzFyLY6v-Gj9QeThGAmExA&type=video&part=snippet&maxResults=4&videoEmbeddable=true&q=+super mario`;
+var youTubeUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBZgbmlJUaRhWn71SGI_EyrsJKuI_A-72Y&type=video&part=snippet&maxResults=4&videoEmbeddable=true&q=+super mario`;
 var wikiUrl = `https://www.mariowiki.com/api.php?action=query&list=search&format=json&srsearch=`
 var searchArray = JSON.parse(localStorage.getItem('userInput')) || [];
 
@@ -21,56 +21,25 @@ var displayVideo = function (object) {
 searchButton.on("click", function (event) {
   event.preventDefault();
   youTubeContainer.html('')
-  var inputValue = inputEl.val()
-  queryUrl1 = youTubeUrl + inputValue;
-  // localStorage.setItem("userInput", JSON.stringify(inputValue))
-  // var data = JSON.parse(localStorage.getItem('userInput'))
-  //  for (var i = searchArray.length - 1; i >= 0; i--) {
-  //  var makeButton = $('searchHistoryButton')
-  //  makeButton.textContent = searchArray[i];
-  //  historyContainer.append(makeButton)
-   
-   var renderBtns = function(searchArray) {
-     searchArray.push('inputValue');
-     localStorage.setItem("userInput", JSON.stringify(inputValue));
-     searchArray.forEach(makeButton);
-      var makeButton = $('searchHistoryButton')
-      makeButton.html(searchArray[i]);
-      historyContainer.append(makeButton);
-   
+  var inputValue = inputEl.val();
+  localStorage.setItem("userInput", JSON.stringify(searchArray));
+  var makeButton = $("<button>");
+  makeButton.attr("class", "button");
+  makeButton.attr(
+    "style",
+    "margin: 2px; border-radius: 10px; color: #145dac; background: #fbd10b");
+  makeButton.text(inputValue);
+  historyContainer.append(makeButton);
 
-   renderBtns();
-  // for (var i = searchHistoryButton.length - 1; i >= 0; i--) {
-  //   var btn = document.createElement('button');
-  //   btn.setAttribute('type', 'button');
-  //   btn.classList.add('history-btn', 'btn-history');
-  //   // `data-search` allows access to city name when click handler is invoked
-  //   btn.setAttribute('data-search', searchHistory[i]);
-  //   btn.textContent = searchHistory[i];
-  //   searchHistoryContainer.append(btn);
-  }
+  queryUrl = youTubeUrl + inputValue;
 
-  fetch(queryUrl1)
+  fetch(queryUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
       $.each(response.items, function (index, item) {
         displayVideo(item)
-      })
-    });
-    wikiContainer.html('')
-  queryUrl2 = wikiUrl + inputValue;
-
-  // fetch data from youtube api
-  fetch(queryUrl2)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      const wikiResults = response.query.search.splice(0, 4)
-      wikiResults.forEach(wikiTitle => {
-        displayWiki(wikiTitle)
       })
     });
 });
@@ -89,6 +58,23 @@ var displayWiki = function (wiki) {
   wikiContainer.append(wikiTitle);
   displaySnippet(wiki)
 }
+searchButton.on("click", function (event) {
+  event.preventDefault();
+  wikiContainer.html('')
+  var inputValue = inputEl.val()
+  queryUrl = wikiUrl + inputValue;
+  // fetch data from youtube api
+  fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      const wikiResults = response.query.search.splice(0, 4)
+      wikiResults.forEach(wikiTitle => {
+        displayWiki(wikiTitle)
+      })
+    });
+});
 
 //this is for the scrolling background
 document.getElementById("body").onscroll = function scrollingBackgound() {
@@ -100,3 +86,6 @@ document.getElementById("body").onscroll = function scrollingBackgound() {
   target.style.backgroundPosition = xvalue + " " + yvalue + "px";
 }
 //scrolling background end
+
+
+
